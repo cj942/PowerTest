9300a911c02fbe039758edf158018332e2445bd5
caojie <caojie@hollysys.com>
2013-05-23 11:34:41 +0800

    someething useful
    Signed-off-by: caojie <caojie@hollysys.com>

--- com/comDlg.cpp
+++ com/comDlg.cpp
@@ -132,7 +132,7 @@ BOOL CComDlg::OnInitDialog()
 	}
 
 	// Set the icon for this dialog.  The framework does this automatically
-	//  when the application's main window is not a dialog
+	// when the application's main window is not a dialog
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
 	
